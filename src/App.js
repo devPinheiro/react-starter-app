def982a026910a84db70f8e4fff94c4623053526
devPinheiro <pinheirolaoluwa@gmail.com>
2019-07-25 21:58:52 +0100

    configure webpack with react with support for proposal class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+    state = {
+        counter: 0
+    }
     render() {
         debugger
         return (
             <div>
-                <h1>React Starter App</h1>
+                <p>Counter: {this.state.counter}</p>
+                <h1 onClick={() => this.setState( state => ({ counter: state.counter + 1}))
+                }
+                >React Starter App</h1>
+                
             </div>
+           
         )
     }
 }
