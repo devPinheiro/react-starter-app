dc2fb32d739283d5744881dc3d97f26e52e8e6ed
devPinheiro <pinheirolaoluwa@gmail.com>
2019-07-25 20:14:34 +0100

    configure webpack with babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = {
     entry: './src/shoutout.js',
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'app.bundle.js'
+    },
+    module:{
+      rules: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              options: {
+                  presets: ['@babel/preset-env']
+              }
+          }
+      ]
     }
 }
