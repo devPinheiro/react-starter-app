313dddd444b27bbe5a2cff5c3483592529994e56
devPinheiro <pinheirolaoluwa@gmail.com>
2019-07-26 13:11:13 +0100

    configure webpack with react and jest setup

---
+++ src/App.spec.js
@@ -0,0 +1,8 @@
+describe('Jest setup', () => {
+    it('Should equal false', () => {
+       expect(false).toBe(false);
+    });
+    it('Should equal true', () => {
+        expect(true).toBe(true);
+    });
+})
